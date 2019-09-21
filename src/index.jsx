c3509ec85f599632e30e94bba49e078875f2fac7
Eric Boutueil <eboutueil@marsatwork.fr>
2019-09-21 22:48:45 +0200

    hello & click react app

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,51 @@
-import React from 'react';
+// import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+// WITHOUT STATE
+// const Hello = ({ name }) => {
+//   return (
+//     <div>
+//       Hello, {name}
+//     </div>
+//   );
+// };
+
+// WITH STATE
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clicked: false
+    }
+
+  }
+
+  // handleClick() { // not sufficient to use this as the component
+  handleClick = () => { // this is not bound correctly to the component
+    // Change the state
+    // console.log('clicked')
+    // this.state.clicked = !this.state.clicked; // cannot change the state directly !
+    this.setState({ // change state + re-trigger render
+      clicked: !this.state.clicked
+    });
+  }
+
+  render(){
+    // BUILD AND RETURN HTML
+    return (
+      <div
+        className={this.state.clicked ? 'clicked' : null}
+        onClick={this.handleClick} // need call a function, a callback, in the class
+      >
+        Hello, {this.props.name}
+      </div> // need use this
+    )
+  }
+}
 
 const root = document.getElementById('root');
 if (root) {
