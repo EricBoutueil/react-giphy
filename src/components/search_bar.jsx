943c444875ebb5afac37c19ab5524c0841474cb3
Eric Boutueil <eboutueil@marsatwork.fr>
2019-09-22 11:07:39 +0200

    controlled components

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,14 +1,29 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+  constructor(props) { // used to access the value of an input, if want to do validation, or have diff inputs
+    super(props);
+
+    this.state = {
+      term: ""
+    }
+
+  }
+
   handleUpdate = (event) => {
+    this.setState({
+      term: event.target.value
+    });
     // console.log(event.target.value); // -> need call the giphy API -> will change state of App -> function in App
     this.props.searchfFunction(event.target.value)
   }
 
   render() {
     return (
-      <input type="text" className="from-control form-search"
+      <input
+        value={this.state.term}
+        type="text"
+        className="from-control form-search"
         onChange={this.handleUpdate}/>
     );
   }
