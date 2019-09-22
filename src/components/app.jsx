af059b80cd4e3bbb590893647b044cd08e728abf
Eric Boutueil <eboutueil@marsatwork.fr>
2019-09-22 09:38:20 +0200

    dynamic gif list

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,24 +1,56 @@
 import React, {Component} from 'react';
+import giphy from 'giphy-api';
+
 import SearchBar from './search_bar';
 import Gif from './gif';
 import GifList from './gif_list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectGifId: "xT9IgDEI1iZyb2wqo8"
+    }
+
+    // this.search("homer"); // simulate typing / the call to search
+  }
+
+  search = (query) => {
+    // API call
+    giphy('jyzyg8sfISDo2hPgr6R1854oRsWDVtsf').search({
+      q: query,
+      rating: 'g',
+      limit: 5
+    // }, function (error, result) {
+    }, (error, result) => {
+      // console.log(result)
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
   render() {
-    const gifs = [
-      {id: "xT9IgDEI1iZyb2wqo8"},
-      {id: "12NUbkX6p4xOO4"}
-    ];
+    // const gifs = [ // prev: props because static -> then state to update with input
+    //   {id: "xT9IgDEI1iZyb2wqo8"},
+    //   {id: "12NUbkX6p4xOO4"}
+    // ];
     return (
       <div>
         <div className="left-scene">
-          <SearchBar/>
+          <SearchBar searchfFunction={this.search}/>
+            {//--> App will pass to SearchBar the search function
+            }
           <div className="selected-gif">
-            <Gif id="xT9IgDEI1iZyb2wqo8"/>
+            {//<Gif id="xT9IgDEI1iZyb2wqo8"/> // static -> static
+            }
+            <Gif id={this.state.selectGifId}/>
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs}/>
+          <GifList gifs={this.state.gifs}/>
         </div>
       </div>
     );
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+  handleUpdate = (event) => {
+    // console.log(event.target.value); // -> need call the giphy API -> will change state of App -> function in App
+    this.props.searchfFunction(event.target.value)
+  }
+
   render() {
     return (
-      <input type="text" className="from-control form-search"/>
+      <input type="text" className="from-control form-search"
+        onChange={this.handleUpdate}/>
     );
   }
 }
